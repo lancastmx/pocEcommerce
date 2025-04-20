09bd3763c8adeb56bca915399d5f7bb6df2cca38
lancast <angelprep92@gmail.com>
2025-04-19 23:00:16 -0600

    feat:Clase 33 de 71  •Uso de Outputs en Angular

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input() img: string = "";
+  @Input() price: number = 0;
+  @Input() title: string = "";
+  @Output() addToCard = new EventEmitter();
 
+  addToCardHandler(){
+    console.log('Hola pa')
+    this.addToCard.emit('ya me escuchas padre')
+  }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from '../../components/product/product.component';
 @Component({
   selector: 'app-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,
+    ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  fromChild(event: string){+
+    console.log('Te escucho hijo');
+    console.log(event);
+  }
 }
