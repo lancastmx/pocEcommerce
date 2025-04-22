028c675fd3e5a40935d33073b14e71af6306fbfa
Lancast <angelprep92@gmail.com>
2025-04-22 11:12:17 -0600

    feat:Clase 44 de 71 • Creando el carrito de compras

--- src/app/domains/shared/componenets/header/header.component.ts
+++ src/app/domains/shared/componenets/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+ hideSideMenu = signal(true);
+   toggleSideMenu() {
+      this.hideSideMenu.update((prev) => !prev);
+    }
 }
