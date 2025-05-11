d0051060bbb1cfa484f526f27913375a50812aca
lancast <angelprep92@gmail.com>
2025-05-10 22:35:20 -0600

    [feat] 404: roulerlink

--- src/app/domains/info/pages/notFound/notFound.component.ts
+++ src/app/domains/info/pages/notFound/notFound.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'app-notFound',
+  standalone: true,
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './notFound.component.html',
   styleUrls: ['./notFound.component.css']
 })
