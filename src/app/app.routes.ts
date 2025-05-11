644594474e6a5ac0a9b178ccf11f503b1102daf9
lancast <angelprep92@gmail.com>
2025-05-10 22:28:33 -0600

    [feat] 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from './domains/info/pages/about/about.component';
+import { NotFoundComponent } from './domains/info/pages/notFound/notFound.component';
 export const routes: Routes = [
   {
     path: '',
@@ -9,5 +10,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
