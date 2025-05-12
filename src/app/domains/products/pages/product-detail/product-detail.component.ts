a3afff23bfb0fa5cbd49017c8bb22413c6e371fb
lancast <angelprep92@gmail.com>
2025-05-11 21:29:50 -0600

    [feat] Clase 63 de 71 •Galería de Productos Dinámica con Angular y TypeScript

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, Input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../../shared/services/product.service';
+import { Product } from '../../../shared/components/counter/models/product.model';
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  @Input({required: true}) id!: number;
+  private productService = inject(ProductService);
+ product = signal<Product | null>(null);
 
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getOne(this.id)
+      .subscribe({
+        next: (product) => {
+          this.product.set(product);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      })
+  }}
 }
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -10,9 +10,9 @@ export class ProductService {
   constructor() { }
 
   getProducts() {
-    return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    return this.http.get<Product[]>(`https://api.escuelajs.co/api/v1/products`)
   }
   getOne(id:number) {
-    return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products/${id}')
+    return this.http.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`)
   }
 }
